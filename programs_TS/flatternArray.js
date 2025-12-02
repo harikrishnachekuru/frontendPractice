69800556d02f967b0388595183aa1ddd42b88403
harikrishnachekuru <chekuruharikrishna@gmail.com>
2025-12-02 10:04:11 +0530

    flatten Array Code practice

---
+++ programs_TS/flatternArray.js
@@ -0,0 +1,32 @@
+// Create a program that clears flatternArray 
+// create using both build-in and without build-in Methods.
+function flatternArray(arr, result) {
+    if (result === void 0) { result = []; }
+    for (var i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === "number") {
+            result.push(arr[i]);
+        }
+        else {
+            flatternArray(arr[i], result);
+        }
+    }
+    return result;
+}
+console.log(flatternArray([1, [2, 3, [4, 5, 6]]]));
+// Using build-in Method
+function flattern(arr) {
+    return arr.flat(Infinity);
+}
+console.log(flattern([1, [2, 3, [4, 5, 6]]]));
+// Using Reduce Method
+function flatArr(arr) {
+    return arr.reduce(function (acc, value) {
+        return acc.concat(Array.isArray(value) ? flatArr(value) : value);
+    }, []);
+}
+console.log(flatArr([1, [2, 3, [4, 5, 6]]]));
+//Using Arr.Prototype Method by using bind method / just reduce it will clear only one step 
+function flatternArr(arr) {
+    return Array.prototype.concat.apply([], arr);
+}
+console.log(flatternArr([1, [2, 3, [4, 5, 6]]]));
